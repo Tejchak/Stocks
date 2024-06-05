f166f5b279195d2e348ffdf4f264ee93b410b49e
Jake Oakley <jakeoakley@Jakes-MacBook-Air.local>
2024-06-04 20:56:54 -0400

    get stocks from csv fixing 2

--- test/StockModelTest.java
+++ test/StockModelTest.java
@@ -1,6 +1,7 @@
 import org.junit.Test;
 
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 
 import static org.junit.Assert.*;
 
@@ -9,7 +10,6 @@ public class StockModelTest {
   @Test
   public void testStockModel() throws FileNotFoundException {
     StockModel model = new StockModel();
-    model.getStockDataCSV("AMC");
-
+    System.out.print(Arrays.toString(model.getStockDataCSV("AMC")));
   }
 }

3348486060d4db79fb5ad450ee4186f3ecd0d2f7
Jake Oakley <jakeoakley@Jakes-MacBook-Air.local>
2024-06-04 21:34:40 -0400

    csv

--- src/StockControllerImpl.java
+++ src/StockControllerImpl.java
@@ -1,4 +1,4 @@
-
+import java.io.FileNotFoundException;
 
 public class StockControllerImpl implements StockController{
   private final StockModel model;
@@ -59,7 +59,7 @@ public class StockControllerImpl implements StockController{
     String stockSymbol = view.getStockSymbol();
     String startDate = view.getDate("start");
     String endDate = view.getDate("end");
-    double gainLoss = model.stockGainLoss(model.getStockData(stockSymbol), startDate, endDate);
+    double gainLoss = model.stockGainLoss(model.getStockDataCSV(stockSymbol), startDate, endDate);
     view.displayResult("The gain/loss over that period of time is " + gainLoss);
   }
 
@@ -67,7 +67,7 @@ public class StockControllerImpl implements StockController{
     String stockSymbol = view.getStockSymbol();
     String startDate = view.getDate("start");
     int xDays = view.getXDays();
-    double movingAverage = model.movingAverage(model.getStockData(stockSymbol), startDate, xDays);
+    double movingAverage = model.movingAverage(model.getStockDataCSV(stockSymbol), startDate, xDays);
     view.displayResult("The " + xDays + "-day moving average is " + movingAverage);
   }
 
@@ -76,7 +76,7 @@ public class StockControllerImpl implements StockController{
     String startDate = view.getDate("start");
     String endDate = view.getDate("end");
     int xDays = view.getXDays();
-    StringBuilder crossovers = model.xDayCrossover(model.getStockData(stockSymbol), startDate, endDate, xDays);
+    StringBuilder crossovers = model.xDayCrossover(model.getStockDataCSV(stockSymbol), startDate, endDate, xDays);
     if (crossovers.length() >= 2) {
       crossovers.delete(crossovers.length() - 2, crossovers.length());
     }
--- test/StockModelTest.java
+++ test/StockModelTest.java
@@ -10,6 +10,8 @@ public class StockModelTest {
   @Test
   public void testStockModel() throws FileNotFoundException {
     StockModel model = new StockModel();
-    System.out.print(Arrays.toString(model.getStockDataCSV("AMC")));
+    System.out.print(model.getStockDataCSV("AMC")[0]);
+    System.out.print(model.getStockDataCSV("AMC")[1]);
+    System.out.print(model.getStockDataCSV("AMC")[2]);
   }
 }

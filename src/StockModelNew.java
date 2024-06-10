a7360175801c3c12fecea7d415ac681727a0082b
Jake Oakley <jakeoakley@Jakes-MacBook-Air.local>
2024-06-09 20:55:02 -0400

    new stock model

--- src/StockModelNew.java
+++ src/StockModelNew.java
@@ -1,3 +1,15 @@
-public class StockModelNew extends StockModelImpl{
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
+public class StockModelNew extends StockModelImpl {
+  private final String apiKey;
+  private final ArrayList<BetterPortfolio> portfolios;
+  private Map<String, String[]> stocks;
+
+  public StockModelNew() {
+    this.apiKey = "QCLLY08TISZBMXL9";
+    this.portfolios = new ArrayList<>();
+    this.stocks = new HashMap<String, String[]>();
+  }
 }

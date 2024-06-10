a0ac67affa4510dfd6bf218c34120f5161661d4d
Jake Oakley <jakeoakley@Jakes-MacBook-Air.local>
2024-06-09 20:46:56 -0400

    comments

--- src/BetterPortfolio.java
+++ src/BetterPortfolio.java
@@ -2,12 +2,21 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Class representing an improved portfolio, this tracks the purchases
+ * and the stocks they are associated with.
+ */
 public class BetterPortfolio {
   String name;
-  Map<String, ArrayList<StockPurchases>> portfolio;
+  Map<String, ArrayList<StockPurchases>> purchases;
 
+  /**
+   * the constructor for the better portfolio that
+   * initializes the map of purchases.
+   * @param name the name of this portfolio.
+   */
   BetterPortfolio(String name){
     this.name = name;
-    this.portfolio = new HashMap<>();
+    this.purchases = new HashMap<>();
   }
 }
--- src/StockPurchases.java
+++ src/StockPurchases.java
@@ -1,10 +1,18 @@
-
-
+/**
+ * Class representing the purchase of a stock in a portfolio, it contains
+ * the amount of shares purchaseDate and sellDate.
+ */
 public class StockPurchases {
   int shares;
   String purchaseDate;
   String sellDate;
 
+  /**
+   * Constructor for stock purchase that initializes the sellDate
+   * as an empty string.
+   * @param shares the amount of shares being purchased.
+   * @param purchaseDate the date of the purchase.
+   */
   StockPurchases(int shares, String purchaseDate) {
     this.shares = shares;
     this.purchaseDate = purchaseDate;

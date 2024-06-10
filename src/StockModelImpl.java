5706ca8fb07205a13ed4bde5d6aedd002b911604
Tejchak <chakravarthy.t@northeastern.edu>
2024-06-10 10:23:16 -0400

    portfolio changes

--- src/StockModelImpl.java
+++ src/StockModelImpl.java
@@ -146,35 +146,13 @@ public class StockModelImpl implements StockModel {
    */
   @Override
   public boolean checkStockExists(String stockSymbol) {
-    URL url = null;
     try {
-      url = new URL("https://www.alphavantage"
-              + ".co/query?function=TIME_SERIES_DAILY"
-              + "&outputsize=full"
-              + "&symbol"
-              + "=" + stockSymbol + "&apikey="+this.apiKey+"&datatype=csv");
+      getStockData(stockSymbol);
     }
-    catch (MalformedURLException e) {
-      throw new RuntimeException("the alphavantage API has either changed or "
-              + "no longer works");
+    catch (Exception e) {
+      return false;
     }
-
-    InputStream in = null;
-    StringBuilder output = new StringBuilder();
-
-    try {
-
-      in = url.openStream();
-      int b;
-
-      while ((b=in.read())!=-1) {
-        output.append((char)b);
-      }
-    }
-    catch (IOException e) {
-      throw new IllegalArgumentException("No price data found for "+stockSymbol);
-    }
-    return !output.toString().contains("\"Error Message\":");
+    return true;
   }
 
   /**
